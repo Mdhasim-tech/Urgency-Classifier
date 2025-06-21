92e8e6f3dcb06ed1b0aa7746ce34800bfb8ce22f
Mdhasim-tech <131744384+Mdhasim-tech@users.noreply.github.com>
2025-06-21 11:16:40 +0530

    Update page.jsx

--- frontend/app/predict/page.jsx
+++ frontend/app/predict/page.jsx
@@ -7,7 +7,7 @@ export default function Predict() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://127.0.0.1:5000/predict", {
+    const res = await fetch("https://urgency-classifier.onrender.com/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text }),
